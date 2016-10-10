509b3058d1893b4dfb6b1ea40b0e1820d3de3c1f
Gurjinder Baath <gurjinderbaath62@gmail.com>
2016-10-10 16:29:18 -0500

    Earlength file creation

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,4 +1,5 @@
 #version control
+#6
 library(stringr)
 data1 <- read.csv("./data/houseelf_earlength_dna_data_1.csv", header = TRUE)
 gc_content <- function(data1){
@@ -9,6 +10,12 @@ t <- str_length(data2)
 GC <- ((Gs + Cs)/t* 100)
 return(GC)
 }
-gc_content(data1$dnaseq)
+new <- gc_content(data1$dnaseq)
+#7
+source('classify_ear_size.R')
+data3 <- get_size_class(data1$earlength)
+data4 <- data.frame(data1$id, data3, new)
+write.csv(data4, file = "earlength.csv")
+
 
 
